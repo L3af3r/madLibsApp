1925c7a0af4cbc062d05a39835bb03f25891b163
L3af3r <leandrochaconfernandez@gmail.com>
2022-04-11 19:21:53 -0400

    Added mad-lib replacement variables to story

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let userInputs = ['Timmie Willie', 'mouse', 'vegetable basket', 'wakes up', 'party', 'village'];
+
+let originalStory = `${userInputs[0]} is a country ${userInputs[1]} who is accidentally taken to a city in a ${userInputs[2]}. When he ${userInputs[3]}, he finds himself at a ${userInputs[4]} and makes a friend. When he is unable to bear the city life, he returns to his home but invites his friend to the ${userInputs[5]}. When his friend visits him, something similar happens.`;
+
+console.log(originalStory);
+
+
+//let originalStory = `Timmie Willie is a country mouse who is accidentally taken to a city in a vegetable basket. When he wakes up, he finds himself at a party and makes a friend. When he is unable to bear (tolerate or experience) the city life, he returns to his home but invites his friend to the village. When his friend visits him, something similar happens.`
