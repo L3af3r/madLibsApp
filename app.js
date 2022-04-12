ac88d85ad495ad4517ca3b2c7f429a5b20763ca8
L3af3r <leandrochaconfernandez@gmail.com>
2022-04-11 20:20:30 -0400

    Final steps whith bonus

--- app.js
+++ app.js
@@ -10,14 +10,22 @@ let questionCounter = 0;
 let questionsLeft = 5;
 
 for(let i = numberOfQuestions; i >= 0; i--){
-	//console.log(questionArray[questionCounter] + `(${questionsLeft} questions left)`);
-	userInputs.push(prompt(questionArray[questionCounter] + `... (${questionsLeft} questions left)`));
+	let input = prompt(questionArray[questionCounter] + `... (${questionsLeft} questions left)`);
+	console.log(input);
+	if(input === ""){
+		console.log(`In '${questionArray[questionCounter]}...' question, the user didn't write anything`);
+		input = "???";
+	}
+	userInputs.push(input);
 	questionCounter++;
 	questionsLeft--;
 }
 
-let originalStory = `${userInputs[0]} is a country ${userInputs[1]} who is accidentally taken to a city in a ${userInputs[2]}. When he ${userInputs[3]}, he finds himself at ${userInputs[4]} and makes a friend. When he is unable to bear the city life, he returns to his home but invites his friend to the ${userInputs[5]}. When his friend visits him, something similar happens.`;
+let originalStory = `<h2>${userInputs[0]} is a country ${userInputs[1]} who is accidentally taken to a city in a ${userInputs[2]}. When he ${userInputs[3]}, he finds himself at ${userInputs[4]} and makes a friend. When he is unable to bear the city life, he returns to his home but invites his friend to the ${userInputs[5]}. When his friend visits him, something similar happens.</h2>`;
 
 console.log(originalStory);
+document.write(originalStory);
+
+alert('All done! Ready for your totally-accurate, not-at-all confusing history of tech??');
 
 //let originalStory = `Timmie Willie is a country mouse who is accidentally taken to a city in a vegetable basket. When he wakes up, he finds himself at a party and makes a friend. When he is unable to bear (tolerate or experience) the city life, he returns to his home but invites his friend to the village. When his friend visits him, something similar happens.`
